306baead907a2b393bf90fdc34305a13f434f4cb
Evan Relf <evan@evanrelf.com>
2020-02-19 22:03:48 -0800

    Generate points

--- src/BarnsleyFern.hs
+++ src/BarnsleyFern.hs
@@ -4,7 +4,7 @@ module BarnsleyFern (main) where
 import Diagrams.Backend.SVG.CmdLine (B, mainWith)
 import Diagrams.Prelude
 import Relude.Unsafe ((!!))
-import System.Random (RandomGen, randomR)
+import System.Random (RandomGen, getStdGen, randomR)
 
 
 f1 :: (Double, Double) -> (Double, Double)
@@ -31,20 +31,31 @@ f4 (x, y) = (x', y') where
   y' = (0.26 * x) + (0.24 * y) + 0.44
 
 
-choose :: RandomGen g => g -> ((Double, Double) -> (Double, Double), g)
-choose rng = (f, rng') where
-  (roll, rng') = randomR (1, 100) rng
+choose :: Int -> ((Double, Double) -> (Double, Double))
+choose roll = f where
   fs = replicate 1 f1
     <> replicate 85 f2
     <> replicate 7 f3
     <> replicate 7 f4
-  f = fs !! roll
+  f = fs !! (roll - 1)
 
 
-fern :: Diagram B
-fern = circle 1 # fc green
+points :: RandomGen g => g -> Int -> [(Double, Double)]
+points initialRng n = take n (unfoldr build ((0, 0), initialRng)) where
+  build (prevPoint, prevRng) =
+    let
+      (roll, rng') = randomR (1, 100) prevRng
+      p = choose roll prevPoint
+    in
+      Just (p, (p, rng'))
 
 
--- $> :main -o fern.svg -w 400
+fern :: Diagram B -> [(Double, Double)] -> Diagram B
+fern shape ps = position (fmap (\p -> (p2 p, shape)) ps)
+
+
 main :: IO ()
-main = mainWith fern
+main = do
+  rng <- getStdGen
+  let shape = circle 0.01 # fillColor green # lineWidth none
+  mainWith (fern shape (points rng 50000))
