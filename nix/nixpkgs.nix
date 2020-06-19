ccf1ef822a0eadd5c6bf631c5064b40fe0f5545f
Evan Relf <evan@evanrelf.com>
2020-06-18 22:22:05 -0700

    Fix incorrect SHA256 hash for pinned Nixpkgs

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -3,7 +3,7 @@ let
   rev = "8731aaaf8b30888bc24994096db830993090d7c4";
   nixpkgs = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-    sha256 = "16wdsazc7g09ibcxlqsa3kblzhbbpdpb6s29llliybw73cp37b9s";
+    sha256 = "1hcc89rxi47nb0mpk05nl9rbbb04kfw97xfydhpmmgh57yrp3zqa";
   };
 in
   import nixpkgs
